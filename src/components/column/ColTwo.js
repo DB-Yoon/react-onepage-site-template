a080de1a50da340e696f25cb0b7acb54ab4eea47
luckydb <yoon.dbdb@gmail.com>
2019-01-18 03:30:53 +0900

    props modified

--- src/components/column/ColTwo.js
+++ src/components/column/ColTwo.js
@@ -1,31 +1,21 @@
 import React from 'react';
 
-const ColTwo = ({children, SecColOneImg, SecColTwoImg, SecColThreeImg, SecColFourImg, SecColFiveImg}) => (
+const ColTwo = ({SecColOneImg, SecColTwoImg, ColExpOne, ColExpTwo}) => (
 	<div className='col-two'>
 		<div className='column'>
 			<div className='column-img'>
-				<img src={SecColOneImg} alt='이미지1' />
+				<img src={`${SecColOneImg}`} alt='이미지1' />
 			</div>
 			<div className='column-exp'>
-				<p>Lorem ipsum dolor sit amet, 
-				consectetur adipiscing elit, 
-				sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-				Viverra suspendisse potenti nullam ac tortor. 
-				A scelerisque purus semper eget duis at tellus at urna.
-				</p>
+				<p>{`${ColExpOne}`}</p>
 			</div>
 		</div>
 		<div className='column'>
 			<div className='column-img'>
-				<img src={SecColTwoImg} alt='이미지2' />
+				<img src={`${SecColTwoImg}`} alt='이미지2' />
 			</div>			
 			<div className='column-exp'>
-				<p>Sit amet consectetur adipiscing elit pellentesque. 
-				Vitae tempus quam pellentesque nec nam aliquam sem et tortor. 
-				Et pharetra pharetra massa massa ultricies. 
-				Sit amet consectetur adipiscing elit pellentesque. 
-				Ac tortor vitae purus faucibus ornare. 
-				</p>
+				<p>{`${ColExpTwo}`}</p>
 			</div>
 		</div>
 	</div>
