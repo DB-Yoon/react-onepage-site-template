9dfad582e9fd7f96a3092937c346e28e3f684c4d
luckydb <yoon.dbdb@gmail.com>
2019-01-18 14:48:54 +0900

    common 삭제

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,6 @@
 import React                    from 'react';
 import { BrowserRouter }        from 'react-router-dom';
 import App                      from './App';
-import Common                   from './styles/Common.css';
 
 const Root = () => {
     return (
